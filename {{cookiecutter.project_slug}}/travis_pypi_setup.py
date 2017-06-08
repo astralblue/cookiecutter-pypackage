f0587f59427eb3d2ffbee23cbf3d2dbddcab63f0
Eugene M. Kim <eugene.kim@ntti3.com>
2017-06-07 21:03:10 -0700

    Alphabetically order imports per hacking/H306
    tests/test_{{slug}}.py deals with conditional imports and cannot easily
    be fixed.  This will be addressed separately.

--- {{cookiecutter.project_slug}}/travis_pypi_setup.py
+++ {{cookiecutter.project_slug}}/travis_pypi_setup.py
@@ -5,13 +5,14 @@
 
 from __future__ import print_function
 import base64
+from getpass import getpass
 import json
 import os
-from getpass import getpass
-import yaml
-from cryptography.hazmat.primitives.serialization import load_pem_public_key
+
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.asymmetric.padding import PKCS1v15
+from cryptography.hazmat.primitives.serialization import load_pem_public_key
+import yaml
 
 
 try:
